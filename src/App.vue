4cbfbb1df92ea03233394f3d9065ac19199b08cb
woodensRangerWZJ <13029768866@163.com>
2020-11-22 00:23:52 +0800

    头部组件添加

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="container">
+    <global-header :user="user"></global-header>
     <column-list :list="list"></column-list>
   </div>
 </template>
@@ -8,6 +9,12 @@
 import { defineComponent } from "vue";
 import "bootstrap/dist/css/bootstrap.min.css";
 import ColumnList, { ColumnProps } from "@/components/ColumnList.vue";
+import GlobalHeader, { UserProps } from "@/components/GlobalHeader.vue";
+
+const currentUser: UserProps = {
+  isLogin: true,
+  name: "木剑游侠"
+};
 const testData: ColumnProps[] = [
   {
     id: 1,
@@ -22,17 +29,32 @@ const testData: ColumnProps[] = [
     description: "这是的test2专栏，有一段非常有意思的简介，可以更新一下欧",
     avatar:
       "http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100"
+  },
+  {
+    id: 3,
+    title: "test3的专栏",
+    description: "这是的test1专栏，有一段非常有意思的简介，可以更新一下欧",
+    avatar:
+      "http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100"
+  },
+  {
+    id: 4,
+    title: "test4的专栏",
+    description: "这是的test2专栏，有一段非常有意思的简介，可以更新一下欧",
+    avatar: ""
   }
 ];
 
 export default defineComponent({
   name: "App",
   components: {
-    ColumnList
+    ColumnList,
+    GlobalHeader
   },
   setup() {
     return {
-      list: testData
+      list: testData,
+      user: currentUser
     };
   }
 });
