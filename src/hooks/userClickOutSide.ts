cfad2a1d9d979c58e14c0cbfb508589311909934
woodensRangerWZJ <13029768866@163.com>
2020-11-22 19:08:43 +0800

    下拉组件hook封装

---
+++ src/hooks/userClickOutSide.ts
@@ -0,0 +1,23 @@
+import { ref, onMounted, onUnmounted, Ref } from "vue";
+
+const useClickOutSide = (elementRef: Ref<null | HTMLElement>) => {
+  const isClickOutSide = ref(false);
+  const handle = (e: MouseEvent) => {
+    if (!elementRef.value) return;
+    if (elementRef.value.contains(e.target as HTMLElement)) {
+      isClickOutSide.value = false;
+    } else {
+      isClickOutSide.value = true;
+    }
+  };
+  onMounted(() => {
+    document.addEventListener("click", handle);
+  });
+
+  onUnmounted(() => {
+    document.removeEventListener("click", handle);
+  });
+  return isClickOutSide;
+};
+
+export default useClickOutSide;
